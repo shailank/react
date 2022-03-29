a9eae8efbb835ee5f669c6af45af744dc7e4fdf8
shailank123456 <shailanksingh786@gmail.com>
2022-03-29 10:46:35 +0530

    add changes

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -3,7 +3,7 @@
 export const Work = [
     {
         id:1,
-        name:"Portfolio Reacg",
+        name:"Portfolio React",
         description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
         tags:["react","gsap","styledComponents"],
         
