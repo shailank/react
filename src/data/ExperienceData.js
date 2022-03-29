a4a3f878f716ddff3888c83b1b7797e2dd99391d
shailank123456 <shailanksingh786@gmail.com>
2022-03-29 10:35:04 +0530

    new changes

--- src/data/ExperienceData.js
+++ src/data/ExperienceData.js
@@ -6,7 +6,7 @@ export const Experiences = [
         tags:["react","Next","styled-components"],
         date:"26 March, 2022",
         imgSrc:"https://financexod.com/wp-content/uploads/2018/09/Kotak-811-Account-min.jpg",
-        link:"k8s-uatfrontend-8c38c640db-1661618154.ap-south-1.elb.amazonaws.com"
+        link:"https://k8s-uatfrontend-8c38c640db-1661618154.ap-south-1.elb.amazonaws.com"
     },
     {
         id:2,
@@ -18,53 +18,38 @@ export const Experiences = [
     },
     {
         id:3,
-        name:"React Loading Screen: Try these 3 cool loading screens for your app",
-        tags:["react","react-lottie","styled-components"],
+        name:"Creating a React.js A",
+        tags:["Vue","Larvel","Rest Api"],
         date:"6 July, 2021",
-        imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625552344293%2Fegwis0UIX.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        link:"https://codebucks.hashnode.dev/react-loading-screens"
+        imgSrc:"https://i.shgcdn.com/30680406-8f56-4bdf-a609-e96a5440f38a/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
+        link:"https://app.fourthfrontier.com/"
     },
     {
         id:4,
-        name:"How to build a Fabulous Todo App using React, Redux and Framer-Motion",
-        tags:["react","redux","framer-motion"],
-        date:"26 May, 2021",
-        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--r5oUDhhT--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/036s7j5b73up7455rdth.png",
-        link:"https://dev.to/codebucks/build-redux-react-todo-list-app-with-animations-using-framer-motion-1mp1"
+        name:"Build a Fabulous Front End website For University ",
+        tags:["react","redux","MUI"],
+        date:"26 May, 2020",
+        imgSrc:"https://studywoo.com/wp-content/uploads/2020/05/K.R.-Mangalam-University-Admission-1024x473.png",
+        link:"https://admissions.krmangalam.edu.in/"
     },
     {
         id:5,
-        name:"What is Higher Order Component (HOC) in React JS?",
+        name:"Ecommer Website with Login and Cart Option",
         tags:["react","reactJS","web-development"],
-        date:"3 Feb, 2021",
-        imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        link:"https://codebucks.hashnode.dev/what-is-higher-order-component"
+        date:"3 Feb, 2019",
+        imgSrc:"https://assets-global.website-files.com/5d779dddc494dd2921fdd1b2/61a782c414e68a855b3c6ae5_responsive-p-500.png",
+        link:"https://www.builder.ai/studio-store-product/ecommerce-app-website"
     },
     {
         id:6,
-        name:"How to implement Pagination Component in React from scratch",
+        name:"Internal Crn for React ",
         tags:["react","css"],
-        date:"5 Jan, 2021",
-        imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625240050668%2FGAya9b0XK.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        link:"https://codebucks.hashnode.dev/pagination-in-react"
+        date:"5 Jan, 2018",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--nCptcTV2--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/qbdaujzokhlc35qgoyjf.png",
+        link:"https://dev.to/sm0ke/react-dashboards-open-source-apps-1c7j"
 
     },
    
-    {
-        id:7,
-        name:"What is Redux ? Simply Explained!",
-        tags:["react","redux","javascript"],
-        date:"16 May, 2021",
-        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.png",
-        link:"https://dev.to/codebucks/what-is-redux-simply-explained-2ch7"
-    },
-    {
-        id:8,
-        name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
-        tags:["react","framer-motion","styled-components"],
-        date:"26 May, 2021",
-        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
-        link:"https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3"
-    },
+   
   
 ] 
--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -3,40 +3,40 @@
 export const Work = [
     {
         id:1,
-        name:"Agency Landing Page",
+        name:"Portfolio Reacg",
         description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
         tags:["react","gsap","styledComponents"],
         
-        demo:"https://agency-website-eta.vercel.app/",
-        github:"https://github.com/codebucks27/Agency-website"
+        demo:"https://shailank-shailank.vercel.app/",
+        github:"https://github.com/shailank/react"
     },
     {
         id:2,
-        name:"Job listing website",
-        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
-        tags:["react","sass"],
+        name:"Wiper Blade",
+        description:"A Product list and detail page with add to cart option with nuy out option",
+        tags:["Vue","sass"],
         
         demo:"http://react-job-listing-website.codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/react-job-listing-website"
+        github:"https://github.com/shailank/wiper-blade"
         
     },
     {
         id:3,
-        name:"Dev.to Clone",
-        description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
+        name:"To Do App",
+        description:"A Todo app which list down all of daily activity task",
         tags:["react","api","sass"],
         
-        demo:"http://devto-clone-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/devto-clone"
+        demo:"https://github.com/shailank/todo-create-user",
+        github:"https://github.com/shailank/todo-create-user"
     },
     {
         id:4,
-        name:"Sidebar Menu",
-        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+        name:"Movies App",
+        description:"A cool looking Movie app build using ReactJS and styledComponents.It has really smooth animations.",
         tags:["react","styledComponents","react-router"],
         
-        demo:"https://react-sidebar.vercel.app/",
-        github:"https://github.com/codebucks27/react-sidebar"
+        demo:"https://github.com/shailank/MovieApp-React",
+        github:"https://github.com/shailank/MovieApp-React"
        
     },{
         id:5,
@@ -44,8 +44,8 @@ export const Work = [
         description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
         tags:["react","redux","framer-motion"],
         
-        demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-Redux-Todo-App"
+        demo:"https://github.com/shailank/reactjstodo",
+        github:"https://github.com/shailank/reactjstodo"
         
     },
     {
@@ -54,8 +54,8 @@ export const Work = [
         description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
         tags:["react","react-router","css"],
         
-        demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-responsive-navbar"
+        demo:"https://github.com/shailank/php-crm",
+        github:"https://github.com/shailank/php-crm"
         
     },
     
